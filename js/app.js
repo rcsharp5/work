7d6450532c2955a61a2a2531f921ba68f770d7fc
rcsharp5 <ryan.sharp5@gmail.com>
2015-02-26 10:14:36 -0500

    Updated

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myapp',[]);
 app.controller("headerctr",function($scope){
+	
 	$scope.title = "Dashboard"
 
 })
 app.directive("headerdir",function(){
 
-	return{templateURL:"templates/header.html"}
+	return{templateUrl:"templates/header.html"}
+
+
+})
+app.directive("dashpanel",function(){
+
+	return{templateUrl:"templates/dashpanels.html"}
 
 
 })
