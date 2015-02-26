d5bea193193324efa945796b4c81910290cfbe62
rcsharp5 <ryan.sharp5@gmail.com>
2015-02-26 09:46:10 -0500

    More angular Changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('myApp',[]);
+app.controller("headerctr",function($scope){
+	$scope.title = "Dashboard"
+
+})
+app.directive("headerdir",function(){
+
+	return{templateURL:"templates/header.html"}
+
+
+})
+
+
